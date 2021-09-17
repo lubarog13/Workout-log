74871b0391ce1b8bb7de657abe8956c903289fea
lubarog13 <lubarog13@gmail.com>
2021-09-17 11:47:05 +0300

    Serializers и немного вьюх

---
+++ eshedule/serializers.py
@@ -0,0 +1,123 @@
+from rest_framework import serializers
+from .models import *
+
+
+class UserSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
+
+
+class MessageSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = "__all__"
+
+
+class CoachSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Coach
+        fields = "__all__"
+
+
+class BuildingSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Building
+        fields = "__all__"
+
+
+class HallSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hall
+        fields = "__all__"
+
+
+class ClubSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Club
+        fields = "__all__"
+
+
+class SignUpSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SignUp
+        fields = "__all__"
+
+
+class WorkoutSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Workout
+        fields = "__all__"
+
+
+class PresenceSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Presence
+        fields = "__all__"
+
+
+class UserSerializer(serializers.ModelSerializer):
+    club = ClubSimpleSerializer(many=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'first_name', 'last_name', 'date_birth', 'sex', 'club']
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    sender = UserSimpleSerializer(many=False)
+    recipient = UserSimpleSerializer(many=False)
+
+    class Meta:
+        model = Message
+        fields = "__all__"
+
+
+class CoachSerializer(serializers.ModelSerializer):
+    user = UserSimpleSerializer(many=False)
+
+    class Meta:
+        model = Coach
+        fields = "__all__"
+
+
+class HallSerializer(serializers.ModelSerializer):
+    building = BuildingSimpleSerializer(many=False)
+
+    class Meta:
+        model = Hall
+        fields = "__all__"
+
+
+class ClubSerializer(serializers.ModelSerializer):
+    coach = CoachSerializer(many=False)
+    building = BuildingSimpleSerializer(many=False)
+
+    class Meta:
+        model = Club
+        fields = "__all__"
+
+
+class SignUpSerializer(serializers.ModelSerializer):
+    club = ClubSimpleSerializer(many=False)
+    user = UserSimpleSerializer(many=False)
+
+    class Meta:
+        model = SignUp
+        fields = "__all__"
+
+
+class WorkoutSerializer(serializers.ModelSerializer):
+    hall = HallSerializer(many=False)
+
+    class Meta:
+        model = Workout
+        fields = "__all__"
+
+class PresenceSerializer(serializers.ModelSerializer):
+    user = UserSimpleSerializer(many=False)
+    workout = WorkoutSerializer(many=False)
+
+    class Meta:
+        model = Presence
+        fields = "__all__"
--- eshedule/views.py
+++ eshedule/views.py
@@ -1,3 +1,91 @@
+from rest_framework.views import APIView
+from rest_framework.generics import CreateAPIView, ListAPIView, UpdateAPIView, RetrieveAPIView
+from .serializers import *
+from rest_framework.response import Response
 from django.shortcuts import render
 
-# Create your views here.
+
+class UserCreateAPIView(CreateAPIView):
+    serializer_class = UserSimpleSerializer
+    queryset = User.objects.all()
+
+
+class MessageCreateAPIView(CreateAPIView):
+    serializer_class = MessageSimpleSerializer
+    queryset = Message.objects.all()
+
+
+class BuildingCreateAPIView(CreateAPIView):
+    serializer_class = BuildingSimpleSerializer
+    queryset = Building.objects.all()
+
+
+class HallCreateAPIView(CreateAPIView):
+    serializer_class = HallSimpleSerializer
+    queryset = Hall.objects.all()
+
+
+class SignUpCreateAPIView(CreateAPIView):
+    serializer_class = SignUp
+    queryset = SignUp.objects.all()
+
+
+class ClubCreateAPIView(CreateAPIView):
+    serializer_class = ClubSimpleSerializer
+    queryset = Club.objects.all()
+
+
+class WorkoutCreateAPIView(CreateAPIView):
+    serializer_class = WorkoutSimpleSerializer
+    queryset = Workout.objects.all()
+
+
+class PresenceCreateAPIView(CreateAPIView):
+    serializer_class = PresenceSimpleSerializer
+    queryset = Presence.objects.all()
+
+
+class UserUpdateAPIView(UpdateAPIView):
+    serializer_class = UserSimpleSerializer
+    queryset = User.objects.all()
+
+
+class MessageUpdateAPIView(UpdateAPIView):
+    serializer_class = MessageSimpleSerializer
+    queryset = Message.objects.all()
+
+
+class BuildingUpdateAPIView(UpdateAPIView):
+    serializer_class = BuildingSimpleSerializer
+    queryset = Building.objects.all()
+
+
+class HallUpdateAPIView(UpdateAPIView):
+    serializer_class = HallSimpleSerializer
+    queryset = Hall.objects.all()
+
+
+class SignUpUpdateAPIView(UpdateAPIView):
+    serializer_class = SignUp
+    queryset = SignUp.objects.all()
+
+
+class ClubUpdateAPIView(UpdateAPIView):
+    serializer_class = ClubSimpleSerializer
+    queryset = Club.objects.all()
+
+
+class WorkoutUpdateAPIView(UpdateAPIView):
+    serializer_class = WorkoutSimpleSerializer
+    queryset = Workout.objects.all()
+
+
+class PresenceUpdateAPIView(UpdateAPIView):
+    serializer_class = PresenceSimpleSerializer
+    queryset = Presence.objects.all()
+
+class TrainingsForUser(APIView):
+    def get(self, request, user_id):
+        trainings = SignUp.objects.filter(user=user_id)
+        serializer = SignUpSerializer(trainings, many=True)
+        return Response({"Trainings: ", serializer.data})
